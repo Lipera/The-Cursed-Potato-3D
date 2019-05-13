7cf32c2e08a0fe4b4155c85535fa9f3910026b08
João Pedro Pereira <joao.pedro.pereira@gmail.com>
2019-05-14 00:05:59 +0100

    [IMPROV] Reconfigured unity rendering pipeline model and added simple yet still not good player controller

---
+++ The-Cursed-Potato-3D/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public float rotationRate = 360;
+    public float moveSpeed = 10;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+    }
+
+    private void ApplyInput(float moveInput, float turnInput) {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+
+    private void Move(float input) {
+        rb.AddForce(transform.forward * input * moveSpeed, ForceMode.Force);
+    }
+
+    private void Turn(float input) {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
