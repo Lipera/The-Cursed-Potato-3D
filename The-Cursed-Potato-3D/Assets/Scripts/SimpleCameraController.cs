f844f4669317e9473980d1cfe684dfa1ba1da9b1
keyaku <antonio.luis.sarmento@gmail.com>
2019-05-26 00:07:32 +0100

    [IMPROV] [SCRIPTS] - Iterating a HashMap of keys instead of an if_else ladder.

--- The-Cursed-Potato-3D/Assets/Scripts/SimpleCameraController.cs
+++ The-Cursed-Potato-3D/Assets/Scripts/SimpleCameraController.cs
@@ -1,169 +1,160 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace UnityTemplateProjects
 {
-    public class SimpleCameraController : MonoBehaviour
-    {
-        class CameraState
-        {
-            public float yaw;
-            public float pitch;
-            public float roll;
-            public float x;
-            public float y;
-            public float z;
+	public class SimpleCameraController : MonoBehaviour
+	{
+		class CameraState
+		{
+			public float yaw;
+			public float pitch;
+			public float roll;
+			public float x;
+			public float y;
+			public float z;
 
-            public void SetFromTransform(Transform t)
-            {
-                pitch = t.eulerAngles.x;
-                yaw = t.eulerAngles.y;
-                roll = t.eulerAngles.z;
-                x = t.position.x;
-                y = t.position.y;
-                z = t.position.z;
-            }
+			public void SetFromTransform(Transform t)
+			{
+				pitch = t.eulerAngles.x;
+				yaw = t.eulerAngles.y;
+				roll = t.eulerAngles.z;
+				x = t.position.x;
+				y = t.position.y;
+				z = t.position.z;
+			}
 
-            public void Translate(Vector3 translation)
-            {
-                Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
+			public void Translate(Vector3 translation)
+			{
+				Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
 
-                x += rotatedTranslation.x;
-                y += rotatedTranslation.y;
-                z += rotatedTranslation.z;
-            }
+				x += rotatedTranslation.x;
+				y += rotatedTranslation.y;
+				z += rotatedTranslation.z;
+			}
 
-            public void LerpTowards(CameraState target, float positionLerpPct, float rotationLerpPct)
-            {
-                yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
-                pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
-                roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
-                
-                x = Mathf.Lerp(x, target.x, positionLerpPct);
-                y = Mathf.Lerp(y, target.y, positionLerpPct);
-                z = Mathf.Lerp(z, target.z, positionLerpPct);
-            }
+			public void LerpTowards(CameraState target, float positionLerpPct, float rotationLerpPct)
+			{
+				yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
+				pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
+				roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
 
-            public void UpdateTransform(Transform t)
-            {
-                t.eulerAngles = new Vector3(pitch, yaw, roll);
-                t.position = new Vector3(x, y, z);
-            }
-        }
-        
-        CameraState m_TargetCameraState = new CameraState();
-        CameraState m_InterpolatingCameraState = new CameraState();
+				x = Mathf.Lerp(x, target.x, positionLerpPct);
+				y = Mathf.Lerp(y, target.y, positionLerpPct);
+				z = Mathf.Lerp(z, target.z, positionLerpPct);
+			}
 
-        [Header("Movement Settings")]
-        [Tooltip("Exponential boost factor on translation, controllable by mouse wheel.")]
-        public float boost = 3.5f;
+			public void UpdateTransform(Transform t)
+			{
+				t.eulerAngles = new Vector3(pitch, yaw, roll);
+				t.position = new Vector3(x, y, z);
+			}
+		}
 
-        [Tooltip("Time it takes to interpolate camera position 99% of the way to the target."), Range(0.001f, 1f)]
-        public float positionLerpTime = 0.2f;
+		CameraState m_TargetCameraState = new CameraState();
+		CameraState m_InterpolatingCameraState = new CameraState();
 
-        [Header("Rotation Settings")]
-        [Tooltip("X = Change in mouse position.\nY = Multiplicative factor for camera rotation.")]
-        public AnimationCurve mouseSensitivityCurve = new AnimationCurve(new Keyframe(0f, 0.5f, 0f, 5f), new Keyframe(1f, 2.5f, 0f, 0f));
+		private Dictionary<KeyCode,Vector3> keys = new Dictionary<KeyCode,Vector3>() {
+			{ KeyCode.W, Vector3.forward },
+			{ KeyCode.S, Vector3.back    },
+			{ KeyCode.A, Vector3.left    },
+			{ KeyCode.D, Vector3.right   },
+			{ KeyCode.Q, Vector3.down    },
+			{ KeyCode.E, Vector3.up      },
+		};
 
-        [Tooltip("Time it takes to interpolate camera rotation 99% of the way to the target."), Range(0.001f, 1f)]
-        public float rotationLerpTime = 0.01f;
+		[Header("Movement Settings")]
+		[Tooltip("Exponential boost factor on translation, controllable by mouse wheel.")]
+		public float boost = 3.5f;
 
-        [Tooltip("Whether or not to invert our Y axis for mouse input to rotation.")]
-        public bool invertY = false;
+		[Tooltip("Time it takes to interpolate camera position 99% of the way to the target."), Range(0.001f, 1f)]
+		public float positionLerpTime = 0.2f;
 
-        void OnEnable()
-        {
-            m_TargetCameraState.SetFromTransform(transform);
-            m_InterpolatingCameraState.SetFromTransform(transform);
-        }
+		[Header("Rotation Settings")]
+		[Tooltip("X = Change in mouse position.\nY = Multiplicative factor for camera rotation.")]
+		public AnimationCurve mouseSensitivityCurve = new AnimationCurve(new Keyframe(0f, 0.5f, 0f, 5f), new Keyframe(1f, 2.5f, 0f, 0f));
 
-        Vector3 GetInputTranslationDirection()
-        {
-            Vector3 direction = new Vector3();
-            if (Input.GetKey(KeyCode.W))
-            {
-                direction += Vector3.forward;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                direction += Vector3.back;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                direction += Vector3.left;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                direction += Vector3.right;
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                direction += Vector3.down;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                direction += Vector3.up;
-            }
-            return direction;
-        }
-        
-        void Update()
-        {
-            // Exit Sample  
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
+		[Tooltip("Time it takes to interpolate camera rotation 99% of the way to the target."), Range(0.001f, 1f)]
+		public float rotationLerpTime = 0.01f;
+
+		[Tooltip("Whether or not to invert our Y axis for mouse input to rotation.")]
+		public bool invertY = false;
+
+		void OnEnable()
+		{
+			m_TargetCameraState.SetFromTransform(transform);
+			m_InterpolatingCameraState.SetFromTransform(transform);
+		}
+
+		Vector3 GetInputTranslationDirection()
+		{
+			Vector3 direction = new Vector3();
+			foreach (KeyValuePair<KeyCode,Vector3> pair in keys) {
+				if (Input.GetKey(pair.Key)) {
+					direction += pair.Value;
+				}
+			}
+			return direction;
+		}
+
+		void Update()
+		{
+			// Exit Sample
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				Application.Quit();
 				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
+				UnityEditor.EditorApplication.isPlaying = false;
 				#endif
-            }
+			}
 
-            // Hide and lock cursor when right mouse button pressed
-            if (Input.GetMouseButtonDown(1))
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-            }
+			// Hide and lock cursor when right mouse button pressed
+			if (Input.GetMouseButtonDown(1))
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+			}
 
-            // Unlock and show cursor when right mouse button released
-            if (Input.GetMouseButtonUp(1))
-            {
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-            }
+			// Unlock and show cursor when right mouse button released
+			if (Input.GetMouseButtonUp(1))
+			{
+				Cursor.visible = true;
+				Cursor.lockState = CursorLockMode.None;
+			}
 
-            // Rotation
-            if (Input.GetMouseButton(1))
-            {
-                var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
-                
-                var mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
+			// Rotation
+			if (Input.GetMouseButton(1))
+			{
+				var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
 
-                m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
-                m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
-            }
-            
-            // Translation
-            var translation = GetInputTranslationDirection() * Time.deltaTime;
+				var mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
 
-            // Speed up movement when shift key held
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                translation *= 10.0f;
-            }
-            
-            // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
-            boost += Input.mouseScrollDelta.y * 0.2f;
-            translation *= Mathf.Pow(2.0f, boost);
+				m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
+				m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
+			}
 
-            m_TargetCameraState.Translate(translation);
+			// Translation
+			var translation = GetInputTranslationDirection() * Time.deltaTime;
 
-            // Framerate-independent interpolation
-            // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
-            var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
-            var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
-            m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+			// Speed up movement when shift key held
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				translation *= 10.0f;
+			}
 
-            m_InterpolatingCameraState.UpdateTransform(transform);
-        }
-    }
+			// Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
+			boost += Input.mouseScrollDelta.y * 0.2f;
+			translation *= Mathf.Pow(2.0f, boost);
+
+			m_TargetCameraState.Translate(translation);
+
+			// Framerate-independent interpolation
+			// Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
+			var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
+			var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
+			m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+
+			m_InterpolatingCameraState.UpdateTransform(transform);
+		}
+	}
 
 }
